f1cdf64eb6278b76f5c325b2d7076b99c0322f6b
Polina Tretyakova <72070079+nepolya@users.noreply.github.com>
2021-03-12 13:32:01 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,18 @@
+from zipfile import ZipFile
+import json
+file_name='test.json.zip'
+with ZipFile(file_name, 'r') as zip: 
+    filename=zip.extract('test.json')
+
+def find_key():
+    f=open(filename, 'r', encoding='utf-8')
+    text = json.load(f)
+    spisok=[]
+    for k, v in text.items():
+        if isinstance(k, str):
+            spisok.append(k)
+        else:
+            find_key(k)
+    return spisok
+print(find_key())
+f.close()
